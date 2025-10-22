05cea06559b4baee900c84c4efc508c5d3ec10bb
yeeahvi <yeeahvi@yeeahvis-MacBook-Air.local>
2025-10-23 00:59:32 +0300

    add brain-progression

--- src/utils.js
+++ src/utils.js
@@ -23,3 +23,7 @@ export const calculateGCD = (a, b) => {
       }
       return a;
   }
+
+export const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
