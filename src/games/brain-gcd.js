7c1f5cf343e8470c7f85f509237fa818c87f5fe8
yeeahvi <yeeahvi@yeeahvis-MacBook-Air.local>
2025-10-26 22:54:04 +0300

    fix2

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,12 +1,12 @@
-import { getRandomNumber, calculateGCD} from '../utils.js'
+import { getRandomNumber, calculateGCD } from '../utils.js'
 
 export const gameDescription = 'Find the greatest common divisor of given numbers.'
 
 export const generateRound = () => {
   const a = getRandomNumber()
   const b = getRandomNumber()
-  const correctAnswer = calculateGCD(a, b) 
-   const question = `${a} ${b}`
+  const correctAnswer = calculateGCD(a, b)
+  const question = `${a} ${b}`
 
-   return { question, correctAnswer }
+  return { question, correctAnswer }
 }
